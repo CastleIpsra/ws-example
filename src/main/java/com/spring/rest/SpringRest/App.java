9a67c75da1d3756bf8f7887c7e7980c3c92b02a6
gamaliellobato <globato@SoporteCDC>
2018-03-16 11:41:26 -0600

    se quitan comentarios

--- src/main/java/com/spring/rest/SpringRest/App.java
+++ src/main/java/com/spring/rest/SpringRest/App.java
@@ -13,13 +13,7 @@ public class App
     public static void main( String[] args )
     {
        SpringApplication.run(App.class, args);
-       //umta maquina vale chetos
-       //hola amigo
-       //hay una nueva versión 1.0.1_2
-       //no lo se
-       //otro no se   2.10.2.203.  
+       
     }
-    //hola isra saca las chelas
-    //Nel Gama     
-    /// holaaaaaaaaaaaaaaaa
+    
 }
